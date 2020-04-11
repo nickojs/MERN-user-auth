6aed7e2d7e2a7077a5fc6de9244decbf32044cd0
nicko <62088609+nickojs@users.noreply.github.com>
2020-04-11 17:04:53 -0300

    Connected auth with user model

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
 
-router.post('/signup', (req, res, next) => {
-  const { username, email, password } = req.body;
-  const user = { username, email, password };
-  
-  res.status(201).send({ user });
+router.post('/signup', async (req, res, next) => {
+  const { username, email, password } = req.body
+
+  try {
+    const fetchedUser = await User.findOne({ where: { email }});
+    if (fetchedUser !== null) {
+      return res.status(302).send({ 
+        error: true,
+        message: 'user already registered' 
+      });
+    }
+    
+    const user = new User({ username, email, password })
+    user.save()
+      .then(user => res.status(201).send({ user }))
+      .catch(err => console.log(err));  
+
+  } catch (error) {
+    console.log(error);
+  }
+
 })
 
 module.exports = router;
