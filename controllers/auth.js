88e7fd82f32cc27b0968289a5c26c46cfdf108be
nicko <62088609+nickojs@users.noreply.github.com>
2020-04-12 08:22:12 -0300

    Changed some json responses and created route to set a reset token for the user

--- controllers/auth.js
+++ controllers/auth.js
@@ -43,14 +43,39 @@ exports.login = async (req, res, next) => {
     if(!hashedPassword) {
       return res.status(401).send({ 
         error: true,
-        message: 'incorrect password for the user ' + user.username
+        message: 'incorrect password for this user'
       });
     }
   
     return res.status(200).send({ 
-      message: 'user ' + user.username + ' logged in'
+      message: `user ${user.email} logged in`
     });
 
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+exports.setToken = async (req, res, next) => {
+  const userEmail = req.body.email;
+
+  try {
+    const rawToken = await randomBytes(32);
+    const parsedToken = rawToken.toString('hex');
+
+    const user = await User.findOne({ where: { email: userEmail }});
+    if(!user){
+      return res.status(404).send({ 
+        error: true,
+        message: 'user not found in the database' 
+      });
+    }
+    // sets the expiration date to 1 hour from now
+    user.resetTokenExpiration = Date.now() + 3600;
+    user.resetToken = parsedToken;
+    user.save()
+    
+    res.status(201).send({ token: parsedToken });
   } catch (error) {
     console.log(error);
   }
